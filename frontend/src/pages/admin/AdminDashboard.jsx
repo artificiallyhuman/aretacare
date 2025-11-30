37bb7ff35c9fa07e9bc7ab19715a94891246fdab
Rob Whiteman <rob.whiteman@hey.com>
2025-11-30 08:49:31 -0600

    dashboard adjustments

--- frontend/src/pages/admin/AdminDashboard.jsx
+++ frontend/src/pages/admin/AdminDashboard.jsx
@@ -166,7 +166,7 @@ export default function AdminDashboard() {
             loading={loading}
           />
           <MetricCard
-            title="Conversations"
+            title="Messages"
             value={metrics?.conversation_count}
             icon="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
             color="pink"
@@ -177,13 +177,6 @@ export default function AdminDashboard() {
             value={metrics?.journal_count}
             icon="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
             color="cyan"
-            loading={loading}
-          />
-          <MetricCard
-            title="Daily Plans"
-            value={metrics?.daily_plan_count}
-            icon="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"
-            color="indigo"
             loading={loading}
           />
         </div>
