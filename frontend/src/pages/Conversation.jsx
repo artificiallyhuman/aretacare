c73d2b02609294a755135156967d542f7c0d83f3
Rob Whiteman <rob.whiteman@hey.com>
2025-12-02 19:04:06 -0600

    line breaks on mobile

--- frontend/src/pages/Conversation.jsx
+++ frontend/src/pages/Conversation.jsx
@@ -478,7 +478,7 @@ const Conversation = () => {
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
                           </svg>
                           {' '}to upload documents and images.
-                          <br className="hidden md:block"/><br className="hidden md:block"/>Share information like:
+                          <br/><br/>Share information like:
                         </p>
                         <ul className="space-y-1 md:space-y-2 text-xs md:text-sm text-gray-700 dark:text-gray-300">
                           <li className="flex items-start">
